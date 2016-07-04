3d0a4143b4bd55a405136be451a429d9cb1138e1
Echo <zhuyucheng52@outlook.com>
2016-07-04 21:51:23 +0800

    angular init

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+angular.module('app', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ngStorage',
+    'ui.router',
+    'ui.bootstrap',
+    // 'ui.utils',
+    // 'ui.load',
+    // 'ui.jq',
+    'oc.lazyLoad',
+    'ngAnimate',
+    // 'toaster',
+    'ngDialog'
+    // 'smart-msg',
+    // 'pascalprecht.translate'
+]);
